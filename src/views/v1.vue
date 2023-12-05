3fe0b1c45e1d859e3765dedb41571198fb50d231
iRuxu <rx6@qq.com>
2023-12-05 15:25:42 +0800

    fix:table

--- src/views/v1.vue
+++ src/views/v1.vue
@@ -197,7 +197,7 @@ export default {
                 },
                 {
                     key: "table",
-                    val: table,
+                    val: "--[[" + table + "]]",
                 },
             ]);
         },
