5a839301a5842de30855377e8ec41f660e09f573
x3zvawq <x3zvawq@gmail.com>
2024-07-17 10:57:56 +0800

    fix(xlsx parse): 忽视无头列

--- src/components/parser/FileDisplay.vue
+++ src/components/parser/FileDisplay.vue
@@ -64,10 +64,9 @@ export default {
             /* walk every column in the range */
             for (C = range.s.c; C <= range.e.c; ++C) {
                 var cell = sheet[XLSX.utils.encode_cell({ c: C, r: R })]; /* find the cell in the first row */
-
                 var hdr = "UNKNOWN " + C; // <-- replace with your desired default
                 if (cell && cell.t) hdr = XLSX.utils.format_cell(cell);
-
+                else continue;
                 headers.push(hdr);
             }
             this.$store.commit("set", {
